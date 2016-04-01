0536d2c937b3ce7a207610b0a365857072f69794
Tom <tom@carrio.me>
2016-04-01 14:44:48 -0400

    Update import_dbs.sh

--- import_dbs.sh
+++ import_dbs.sh
@@ -13,8 +13,8 @@ for filename in /dbs/migration/backups/yyyy-mm-dd/*.sql
 
 for filename in /dbs/migration/backups/yyyy-mm-dd/*.sql
 	do dbname=$(echo $filename | cut -d '.' -f 1 | cut -d '/' -f 6)
-		if [ -n "$filename" ]; then
-			if (( $(stat -c%s Database.sql) > 161 )); then
+		if [ -n "dbname" ]; then
+			if (( $(stat -c%s $filename) > 161 )); then
 				echo "FILLING DATABASE $dbname"
 				mysql -u root "-pDBPASSWORD" -e "create database if not exists $dbname"
 				mysql -u root "-pDBPASSWORD" $dbname  < $filename
